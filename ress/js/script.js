f6521919d825685f8dd847f694bb2198d633e9b6
EmilyRacheal <emilychealy4@gmail.com>
2021-04-30 13:04:50 +0100

    changed some text colors

--- ress/js/script.js
+++ ress/js/script.js
@@ -1,9 +1,11 @@
 $(window).scroll(function() {
     if ($(this).scrollTop() > 50) {
       $(".navbar").addClass("mycolor");
+      $("nav.fixed-top .nav-link").addClass("mycol");
       $(".navbar").fadeIn();
     } else {
       $(".navbar").removeClass("mycolor");
+      $("nav.fixed-top .nav-link").removeClass("mycol");
       $(".navbar").fadeIn();
     }
 });
